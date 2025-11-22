d9a40d514f1d086e6afc925f39e0823973245c56
ALICTF <alictf1@gmail.com>
2025-11-22 12:34:47 +0330

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://alictf.github.io', 
-  base: '/BlackFriday', 
-
+  site: 'https://ALICTF.github.io',
+  base: '/ArmfitPlusLanding', // <--- این خط باید دقیقاً اسم مخزن جدیدت باشه
+  
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
