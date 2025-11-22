db34c6255a8682c1562aa884dea5a0b0124568ad
ALICTF <alictf1@gmail.com>
2025-11-22 12:23:52 +0330

    BlackFriday page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://alictf.github.io', 
+  base: '/BlackFriday', 
+
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
